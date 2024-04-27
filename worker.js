d401a8de0778069c3a7116c2797d1ce9ffd33e6c
Ransomware Evil <126243832+NiREvil@users.noreply.github.com>
2024-04-27 20:46:31 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -294,7 +294,7 @@ async function handleRequest(request) {
   // 如果path为空, 即直接访问本worker
   // If visit this worker directly (no path)
   if (!path) {
-    return Response.redirect("https://seriously-why.revil.workers.dev", 302)
+    return Response.redirect("https://url.xcs.workers.dev", 302)
     /* new Response(html404, {
       headers: response_header,
       status: 404
